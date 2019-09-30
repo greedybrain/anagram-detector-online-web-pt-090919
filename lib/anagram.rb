07bd58f004e5cf87cced7454d5d1c2c4229666d7
Naya  Willis <greedybrain18@gmail.com>
2019-09-30 23:52:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   
   def match(list_possible_anagrams) 
     count = 0 
+    res = []
     list_possible_anagrams.each do |possible_word|
       possible_word.each_char do |char|
         if @word.include?(char)
@@ -15,6 +16,12 @@ class Anagram
         end
       end
     end
+    if count == @word.length
+      
+    end
   end
   
 end
+
+
+
