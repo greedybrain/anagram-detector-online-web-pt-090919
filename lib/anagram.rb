da1881515d8831525348f26e4571d01ed3544b8b
Naya  Willis <greedybrain18@gmail.com>
2019-10-01 00:12:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,11 @@ class Anagram
   
   def match(list_possible_anagrams)
     count = 0
-    results = list_possible_anagrams.collect do |possible_word|
-      possible_word.each_char do |char|
-        
+    results = list_possible_anagrams.collect do |possible_word| #check each word
+      possible_word.each_char do |char| #each character
+        if @word.include?(char)
+          count += 1
+        end
       end
     end
   end
